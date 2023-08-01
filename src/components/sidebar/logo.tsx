3bbf57f6cdd38bc1756836dc6ac539a92de8de65
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-08-01 19:51:56 -0300

    first commit

---
+++ src/components/sidebar/logo.tsx
@@ -0,0 +1,192 @@
+export function Logo() {
+  return (
+    <strong className="mx-1 flex items-center gap-2 text-xl font-semibold text-zinc-900">
+      <svg width={38} height={38} viewBox="0 0 38 38" fill="none">
+        <g filter="url(#a)">
+          <g clipPath="url(#b)">
+            <rect x={3} y={2} width={32} height={32} rx={8} fill="#fff" />
+            <rect x={3} y={2} width={32} height={32} rx={8} fill="url(#c)" />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M19 4.039c-7.71 0-13.961 6.25-13.961 13.961 0 7.71 6.25 13.961 13.961 13.961 7.71 0 13.961-6.25 13.961-13.961 0-7.71-6.25-13.961-13.961-13.961ZM4.961 18c0-7.753 6.286-14.039 14.039-14.039S33.039 10.247 33.039 18 26.753 32.039 19 32.039 4.961 25.753 4.961 18Z"
+              fill="#D0D5DD"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M19 14.078a3.922 3.922 0 1 0 0 7.845 3.922 3.922 0 0 0 0-7.845ZM15 18a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"
+              fill="#D0D5DD"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M19 15.32a2.68 2.68 0 1 0 0 5.36 2.68 2.68 0 0 0 0-5.36ZM16.243 18a2.757 2.757 0 1 1 5.514 0 2.757 2.757 0 0 1-5.514 0Z"
+              fill="#D0D5DD"
+            />
+            <path d="M18.961 2h.078v32h-.078V2Z" fill="#D0D5DD" />
+            <path d="M35 17.96v.079H3v-.078h32Z" fill="#D0D5DD" />
+            <path
+              d="M29.602 2h.078v32h-.078V2ZM13.64 2h.078v32h-.077V2ZM24.282 2h.077v32h-.077V2ZM8.32 2h.078v32H8.32V2Z"
+              fill="#D0D5DD"
+            />
+            <path
+              d="M35 28.602v.077H3v-.077h32ZM35 12.64v.078H3v-.077h32ZM35 23.281v.078H3v-.078h32ZM35 7.32v.078H3V7.32h32Z"
+              fill="#D0D5DD"
+            />
+            <g filter="url(#d)">
+              <circle cx={19} cy={18} r={8} fill="url(#e)" />
+            </g>
+            <g filter="url(#f)">
+              <path
+                d="M3 18h32v3.2c0 4.48 0 6.72-.872 8.432a8 8 0 0 1-3.496 3.496C28.92 34 26.68 34 22.2 34h-6.4c-4.48 0-6.72 0-8.432-.872a8 8 0 0 1-3.496-3.496C3 27.92 3 25.68 3 21.2V18Z"
+                fill="#fff"
+                fillOpacity={0.2}
+              />
+            </g>
+          </g>
+          <rect
+            x={3.15}
+            y={2.15}
+            width={31.7}
+            height={31.7}
+            rx={7.85}
+            stroke="#D0D5DD"
+            strokeWidth={0.3}
+          />
+        </g>
+        <defs>
+          <filter
+            id="a"
+            x={0}
+            y={0}
+            width={38}
+            height={38}
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy={1} />
+            <feGaussianBlur stdDeviation={1} />
+            <feColorMatrix values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.06 0" />
+            <feBlend
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_402_307"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy={1} />
+            <feGaussianBlur stdDeviation={1.5} />
+            <feColorMatrix values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.1 0" />
+            <feBlend
+              in2="effect1_dropShadow_402_307"
+              result="effect2_dropShadow_402_307"
+            />
+            <feBlend
+              in="SourceGraphic"
+              in2="effect2_dropShadow_402_307"
+              result="shape"
+            />
+          </filter>
+          <filter
+            id="d"
+            x={8}
+            y={8}
+            width={22}
+            height={22}
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy={1} />
+            <feGaussianBlur stdDeviation={1} />
+            <feColorMatrix values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.06 0" />
+            <feBlend
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_402_307"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy={1} />
+            <feGaussianBlur stdDeviation={1.5} />
+            <feColorMatrix values="0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.1 0" />
+            <feBlend
+              in2="effect1_dropShadow_402_307"
+              result="effect2_dropShadow_402_307"
+            />
+            <feBlend
+              in="SourceGraphic"
+              in2="effect2_dropShadow_402_307"
+              result="shape"
+            />
+          </filter>
+          <filter
+            id="f"
+            x={-2}
+            y={13}
+            width={42}
+            height={26}
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation={2.5} />
+            <feComposite
+              in2="SourceAlpha"
+              operator="in"
+              result="effect1_backgroundBlur_402_307"
+            />
+            <feBlend
+              in="SourceGraphic"
+              in2="effect1_backgroundBlur_402_307"
+              result="shape"
+            />
+          </filter>
+          <linearGradient
+            id="c"
+            x1={19}
+            y1={2}
+            x2={19}
+            y2={34}
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff" />
+            <stop offset={1} stopColor="#D0D5DD" />
+          </linearGradient>
+          <linearGradient
+            id="e"
+            x1={15}
+            y1={26}
+            x2={23}
+            y2={10}
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#53389E" />
+            <stop offset={1} stopColor="#6941C6" />
+          </linearGradient>
+          <clipPath id="b">
+            <rect x={3} y={2} width={32} height={32} rx={8} fill="#fff" />
+          </clipPath>
+        </defs>
+      </svg>
+
+      <span>Untitled UI</span>
+    </strong>
+  )
+}
