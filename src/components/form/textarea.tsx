e757cb0751aa0991204e0c2f6e535ac27344812f
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-08-03 21:09:56 -0300

    feat: biography input

---
+++ src/components/form/textarea.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { ComponentProps } from 'react'
+
+interface TextareaProps extends ComponentProps<'textarea'> {}
+
+export function Textarea(props: TextareaProps) {
+  return (
+    <textarea
+      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
