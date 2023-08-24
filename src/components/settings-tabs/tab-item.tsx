e99420b68895103eecd3e2851bb76c45091cebbe
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-08-24 09:55:40 -0300

    feat: creating hover effects

--- src/components/settings-tabs/tab-item.tsx
+++ src/components/settings-tabs/tab-item.tsx
@@ -7,8 +7,10 @@ interface TabItemProps {
 
 export function TabItem({ title, value }: TabItemProps) {
   return (
-    <TabsTrigger className="group" value={value}>
-      <span>{title}</span>
+    <TabsTrigger className="group outline-none" value={value}>
+      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
 
       <div className="absolute -bottom-px left-0 right-0 hidden h-0.5 bg-violet-700 group-data-[state=active]:block" />
     </TabsTrigger>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -28,7 +28,7 @@ export function InputRoot({ className, ...props }: InputRootProps) {
   return (
     <div
       className={cn(
-        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm',
+        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
         className,
       )}
       {...props}
