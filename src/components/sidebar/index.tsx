2a61012d26aafdcfc7fadc9431a49f312b05eb0e
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-08-25 10:44:14 -0300

    feat: sidebar responsiveness

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -9,7 +9,7 @@ import { UsedSpaceWidget } from '@/components/sidebar/used-space-widget'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="fixed inset-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
 
       <InputRoot>
